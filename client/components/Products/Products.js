0a86a83d9021364bcae0e68e9f3c6466d620e6de
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-06-28 22:04:18 -0400

    add product css file

--- client/components/Products/Products.js
+++ client/components/Products/Products.js
@@ -1,8 +1,9 @@
 import PageHeading from '../PageHeading/PageHeading'
+import styles from './Products.module.css'
 
 export default function Products() {
   return (
-    <div>
+    <div className={styles.products_container}>
       <PageHeading heading='My Products' />
       <h2>Products</h2>
     </div>
