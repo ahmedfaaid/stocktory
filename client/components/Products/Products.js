5c18ae51ca9e73a4d19be455e1faf591d678942d
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-07-08 18:53:02 -0400

    Part of product list

--- client/components/Products/Products.js
+++ client/components/Products/Products.js
@@ -1,9 +1,42 @@
 import styles from './Products.module.css'
+import products from '../../productData'
 
 export default function Products() {
   return (
     <div className={styles.products_container}>
-      <h2>Products</h2>
+      <div className={styles.products_list}>
+        <table className={styles.products_table}>
+          <thead>
+            <tr className={styles.table_head}>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              products.map(product => (
+                <tr>
+                  <td>{product.id}</td>
+                  <td>{product.productName}</td>
+                  <td>{product.price.toFixed(2)}</td>
+                  <td>{product.quantity}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+      {/* {products.map(product => (
+        <div key={product.id} className={styles.products_card}>
+          <h3 className={styles.productName}>{product.productName}</h3>
+          <div className={styles.detail_box}>
+            <p className={styles.price}>Unit Price: <span className={styles.value}>${product.price}</span></p>
+            <p className={styles.quantity}>Quantity: <span className={styles.value}>{product.quantity}</span></p>
+          </div>
+        </div>
+      ))} */}
     </div>
   )
 }
